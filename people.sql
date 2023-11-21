60e2710672e1beb11dc3856aee1dd374bbecb3e4
Nadya <65419241+nadyaloseva@users.noreply.github.com>
2023-11-21 12:06:00 +0300

    Create people.sql

---
+++ people.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "people" ("Person" VARCHAR(20),"Region" VARCHAR(10));
+INSERT INTO "people" ("Person","Region") VALUES 
+ ('Anna Andreadi','West'), 
+ ('Chuck Magee','East'), 
+ ('Kelly Williams','Central'), 
+ ('Cassandra Brandow','South');
