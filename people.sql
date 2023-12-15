6e731bb03be8c5d2a85a313aca0ec621d47bfc5e
Nadya <65419241+nadyaloseva@users.noreply.github.com>
2023-12-15 13:37:07 +0300

    Delete people.sql

--- people.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "people" ("Person" VARCHAR(20),"Region" VARCHAR(10));
-INSERT INTO "people" ("Person","Region") VALUES 
- ('Anna Andreadi','West'), 
- ('Chuck Magee','East'), 
- ('Kelly Williams','Central'), 
- ('Cassandra Brandow','South');
