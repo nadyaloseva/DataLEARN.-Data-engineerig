54a432a9f7a35638a510234e861b68f1ba529138
Nadya <65419241+nadyaloseva@users.noreply.github.com>
2023-12-15 14:07:19 +0300

    Add files via upload

---
+++ create_table_dw.sql
@@ -0,0 +1,10 @@
+--создание таблицы total_month
+CREATE TABLE dw.total_month (
+	"type" varchar(40) NULL,
+	month_date date NULL,
+	sales numeric NULL,
+	quantity numeric NULL,
+	discount numeric NULL,
+	profit numeric NULL
+);
+
